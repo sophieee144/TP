56c81012b0782312920cbb0faeedda600341a04c
sophieee144 <bouledroua.zahra@gmail.com>
2024-12-28 14:18:34 +0100

    khra khro khri

--- QCM/app.js
+++ QCM/app.js
@@ -1,34 +1,15 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const questions = require("./qst");
 
 const app = express();
 const PORT = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static("QCM"));
-app.get("/QCM", (req, res) => {
-  res.sendFile(__dirname + "/QCM.html");
-});
+app.use(express.static("public"));
 
-app.get("/question/:id", (req, res) => {
-  const questionId = parseInt(req.params.id);
-  if (questionId < questions.length) {
-    res.json(questions[questionId]);
-  } else {
-    res.status(404).send("Question not found");
-  }
-});
 
-app.post("/check-answer/:id", (req, res) => {
-  const questionId = parseInt(req.params.id);
-  const userAnswer = parseInt(req.body.answer);
-
-  if (userAnswer === questions[questionId].correct) {
-    res.json({ correct: true });
-  } else {
-    res.json({ correct: false });
-  }
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/QCM.html');
 });
 
 app.listen(PORT, () => {
---
+++ QCM/public/qst.js
@@ -0,0 +1,27 @@
+const questions = [
+  {
+    question: "What is the capital of France?",
+    answers: ["Berlin", "Madrid", "Paris"],
+    correct: 2, // Index of the correct answer
+  },
+  {
+    question: "Which planet is known as the Red Planet?",
+    answers: ["Earth", "Mars", "Jupiter"],
+    correct: 1, // Index of the correct answer
+  },
+  {
+    question: "What is the largest ocean on Earth?",
+    answers: ["Atlantic Ocean", "Indian Ocean", "Pacific Ocean"],
+    correct: 2, // Index of the correct answer
+  },
+  {
+    question: "Who wrote 'Romeo and Juliet'?",
+    answers: ["Charles Dickens", "William Shakespeare", "Mark Twain"],
+    correct: 1, // Index of the correct answer
+  },
+  {
+    question: "What is the chemical symbol for water?",
+    answers: ["H2O", "O2", "CO2"],
+    correct: 0, // Index of the correct answer
+  },
+];
